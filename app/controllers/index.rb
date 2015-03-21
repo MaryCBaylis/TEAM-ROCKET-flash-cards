656afaf3ba1a5c58f98638034b43550a560aa470
Erin Smith <erinsmith.8585@gmail.com>
2015-03-21 23:12:34 +0000

    modify posting to new account to take email and password

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,7 +9,7 @@ end
 
 post '/account/new' do
   puts "[LOG] responding to a POST request for /account/new"
-  @user = User.new(params[:user])
+  @user = User.new(params[:user][:email], params[:user][:password])
   if @user.save
     redirect '/account'
   else 
