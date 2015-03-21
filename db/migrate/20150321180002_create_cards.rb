d648c87cc342b239298d07943deceee914f2f86d
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-03-21 18:03:43 +0000

    Create cards schema

---
+++ db/migrate/20150321180002_create_cards.rb
@@ -0,0 +1,13 @@
+class CreateCards < ActiveRecord::Migration
+  def change
+    create_table :cards do |t|
+      t.string :question
+      t.string :answer
+      t.integer :pokemon_id
+      t.integer :deck_id
+      t.boolean :correct
+      
+      t.timestamps
+    end
+  end
+end
