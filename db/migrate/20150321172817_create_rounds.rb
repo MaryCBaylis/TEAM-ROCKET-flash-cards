0e16960c90f68fe754b76b426ffd7c2368ba2486
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-03-21 18:33:55 +0000

    Complete migration

--- db/migrate/20150321172817_create_rounds.rb
+++ db/migrate/20150321172817_create_rounds.rb
@@ -2,7 +2,7 @@ class CreateRounds < ActiveRecord::Migration
   def change
     create_table :rounds do |t|
       t.integer :user_id 
-      t.intger :deck_id
+      t.integer :deck_id
       t.string :deck_order
       
       t.timestamps
